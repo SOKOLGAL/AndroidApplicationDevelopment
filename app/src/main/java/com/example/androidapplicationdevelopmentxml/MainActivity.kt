8869b620aca0ee93453648dbba7aeb3c26a09977
Galina <sokolovagb@gmail.com>
2025-07-19 19:25:35 +0300

    RA-03 two buttons icon

--- app/src/main/java/com/example/androidapplicationdevelopmentxml/MainActivity.kt
+++ app/src/main/java/com/example/androidapplicationdevelopmentxml/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidapplicationdevelopmentxml
 
+import com.example.androidapplicationdevelopmentxml.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +8,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
